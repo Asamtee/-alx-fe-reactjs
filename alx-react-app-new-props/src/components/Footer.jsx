3be2d7c71419fc44b94348eacd25f53e29ca881f
ONYEGBULE EBUBECHUKWU <albertsammy07@gmail.com>
2024-08-16 20:35:18 +0100

    Delete alx-react-app-new-props directory error

--- alx-react-app-new-props/src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-<footer style={{ backgroundColor: "navy", color: "white" }}>
-  <p>© 2023 City Lovers</p>
-</footer>;
-
-export default Footer;
--- alx-react-app-new-props/src/components/MainContent.jsx
+++
@@ -1,5 +0,0 @@
-<main style={{ backgroundColor: "white", color: "blue" }}>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>;
-
-export default MainContent;
