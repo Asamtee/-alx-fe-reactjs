c8752d12777a2169e96c4365641933c487fb9af1
ONYEGBULE EBUBE <albertsammy07@gmail.com>
2024-08-31 18:35:54 +0100

    advanced react routing

---
+++ react-router-advanced/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "1.5rem",
+      }}
+    >
+      <Link to="/">Home</Link>
+      <Link to="/Profile">Profile</Link>
+      <Link to="/Blog">Blog</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+
+const Profile = () => {
+  return (
+    <>
+      <div>Profile Page </div>
+      <nav>
+        <Link to="details"> Details </Link>
+        <Link to="settings"> Settings </Link>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Profile;
