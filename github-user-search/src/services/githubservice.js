131b31d31327dc1c4846b272dbcb97b88b64f328
ONYEGBULE EBUBECHUKWU <albertsammy07@gmail.com>
2024-09-26 10:21:32 +0100

    Update githubservice.js

--- github-user-search/src/services/githubservice.js
+++ github-user-search/src/services/githubservice.js
@@ -1,42 +1,42 @@
-import axios from "axios";
-const BASE_URL = "https://api.github.com/search/users?q";
+import axios from 'axios'
+    const BASE_URL = 'https://api.github.com/search/users?q'
 
-export const fetchUserData = async (
-  username,
-  location = "",
-  minRepos = "",
-  perPage = 30,
-  page = 1
-) => {
-  let query = `=${username}`;
+    export const fetchUserData = async(username,location='', minRepos = '', perPage = 30, page = 1) => {
+        let query = `=${username}`;
 
-  if (location) {
-    query += `+location:${location}`;
-  }
+        if (location) {
+            query += `+location:${location}`;
+        }
 
-  if (minRepos) {
-    query += `+repos:>=${minRepos}`;
-  }
+        if (minRepos) {
+            query += `+repos:>=${minRepos}`;
+        }
 
-  try {
-    const response = await axios.get(
-      `${BASE_URL}${query}&per_page=${perPage}&page=${page}`
-    );
-    const users = response.data.items;
+        try{
+            const response = await axios.get(`${BASE_URL}${query}&per_page=${perPage}&page=${page}`);
+            const users = response.data.items;
 
-    const detailedUsers = await Promise.all(
-      users.map(async (user) => {
-        const userDetailsResponse = await axios.get(user.url);
-        return userDetailsResponse.data;
-      })
-    );
+            const detailedUsers = await Promise.all(
+                users.map(async (user) => {
+                    const userDetailsResponse = await axios.get(user.url);
+                    return userDetailsResponse.data;  
+                })
+            );
+    
+            return {
+                total_count: response.data.total_count,
+                users: detailedUsers,
+            };
 
-    return {
-      total_count: response.data.total_count,
-      users: detailedUsers,
+        } catch (err) {
+    
+            if (err.response && err.response.status === 403) {
+                throw new Error('GitHub API rate limit exceeded. Please try again later.');
+            } else if (err.response && err.response.status === 404) {
+                throw new Error('Looks like we cant find the user');
+            } else {
+                throw new Error('Failed to fetch user data. Please try again.');
+            }
+    }
     };
-  } catch (err) {
-    console.log("Error Msg:", err);
-    throw err;
-  }
-};
+  
